6d96fdefb37b6af738ec5f10a06c0a4bbdd3dd84
agent66 <32763420+agent66@users.noreply.github.com>
2017-10-27 14:31:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ window.onload = function() {
 	var symbol;
 	var winner;
 	var gameOver = false;
-	filled = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
-	symbol = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
-	winner = [[0,1,2,3,4], [5,6,7,8,9], [10,11,12,13,14], [15,16,17,18,19], [20,21,22,23,24], [0,5,10,15,20], [1,6,11,16,21], [2,7,12,17,22], [3,8,13,18,23],[4,9,14,19,24],[0,6,12,18,24],[4,8,12,16,20]];
+	filled = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+	symbol = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+	winner = [[0,1,2,3,4,5,6], [7,8,9,10,11,12,13], [14,15,16,17,18,19,20], [21,22,23,24,25,26,27], [28,29,30,31,32,33,34], [35,36,37,38,39,40,41], [42,43,44,45,46,47,48], [0,7,14,21,28,35,42], [1,8,15,22,29,36,43],[2,9,16,23,30,37,44],[3,10,17,24,31,38,45],[4,11,18,25,32,39,46],[5,12,19,26,33,40,47],[6,13,20,27,34,41,48],[0,8,16,24,32,40,48],[6,12,18,24,30,36,42]];
 
 	//2. NewGame - event + function
 	//Create a new game click event
@@ -94,6 +94,54 @@ window.onload = function() {
 							break;
 			case "canvas25": num = 25;
 							break;
+			case "canvas26": num = 26;
+							break;
+			case "canvas27": num = 27;
+							break;
+			case "canvas28": num = 28;
+							break;
+			case "canvas29": num = 29;
+							break;
+			case "canvas30": num = 30;
+							break;
+			case "canvas31": num = 31;
+							break;
+			case "canvas32": num = 32;
+							break;
+			case "canvas33": num = 33;
+							break;
+			case "canvas34": num = 34;
+							break;
+			case "canvas35": num = 35;
+							break;
+			case "canvas36": num = 36;
+							break;
+			case "canvas37": num = 37;
+							break;
+			case "canvas38": num = 38;
+							break;
+			case "canvas39": num = 39;
+							break;
+			case "canvas40": num = 40;
+							break;
+			case "canvas41": num = 41;
+							break;
+			case "canvas42": num = 42;
+							break;
+			case "canvas43": num = 43;
+							break;
+			case "canvas44": num = 44;
+							break;
+			case "canvas45": num = 45;
+							break;
+			case "canvas46": num = 46;
+							break;
+			case "canvas47": num = 47;
+							break;
+			case "canvas48": num = 48;
+							break;
+			case "canvas49": num = 49;
+							break;
 
 
 		}
@@ -128,7 +176,7 @@ window.onload = function() {
 
 				var s = symbol[num-1];
 				for(var j=0; j < winner.length; j++) {
-					if((symbol[winner[j][0]] == s) && (symbol[winner[j][1]] == s) && (symbol[winner[j][2]] == s) && (symbol[winner[j][3]] == s) && (symbol[winner[j][4]] == s)) {
+					if((symbol[winner[j][0]] == s) && (symbol[winner[j][1]] == s) && (symbol[winner[j][2]] == s) && (symbol[winner[j][3]] == s) && (symbol[winner[j][4]] == s) && (symbol[winner[j][5]] == s) && (symbol[winner[j][6]] == s)) {
 						document.getElementById("result").innerText = "Player '" + s + "' won!";
 						gameOver = true;
 					}
@@ -137,7 +185,7 @@ window.onload = function() {
 				//draw condition
 				// turn > 0 gameOver == false
 
-				if(turn > 25 && gameOver != true) {
+				if(turn > 49 && gameOver != true) {
 					document.getElementById("result").innerText = "GAME OVER! IT WAS A DRAW!";
 				}
 			}
